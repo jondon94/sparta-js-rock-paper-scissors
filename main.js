4ac0b1842c3b4c6ebc3199f12f017921f1d7fe48
Tech-A66 <tech-a66@Tech-A66s-Air.home>
2017-10-31 20:59:08 +0000

    managed to repeat the game 5 times, still no score record

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 alert("Would you like to play a game?")
 alert("The Rules are: Rock beats Scissors, Paper Beats Rock and Scissors Beats Paper. Best of 5 rounds win")
+
 var g = 1;
 while (g < 6) {
-  g++
-}
+
 var playerChoice = prompt("Choose Rock, Paper or Scissors")
 var compChoice = Math.random();
   if (compChoice < 0.34){
@@ -13,6 +13,7 @@ var compChoice = Math.random();
   }else{
     compChoice = "Scissors"
   }
+
 var battle = function(weapon1, weapon2){
   if (weapon1 === weapon2){
     return alert("Its a tie")
@@ -25,23 +26,23 @@ var battle = function(weapon1, weapon2){
 
   }else if (weapon1 === "Paper"){
     if (weapon2 === "Rock"){
-      return alert("Player 1 wins")
-    }else {
+      return alert("Player 1 Wins")
+    }else{
       return alert("Computer Wins")
     }
 
   }else if (weapon1 === "Scissors"){
     if (weapon2 === "Paper"){
       return alert("Player 1 Wins")
-    }else {
+    }else{
       return alert("Computer Wins")
     }
   }
-
 }
 
 
-
 battle(playerChoice, compChoice);
-// alert("Player 1: " + playerChoice + " vs " + "Computer" + compChoice
-//   "The Winner is:" + Winner + "Games Played: " + g + ". Score: " + playerChoiceScore + " vs " + compChoiceScore)
+
+
+g++
+}
