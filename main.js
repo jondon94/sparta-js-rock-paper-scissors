aff1a20a4329c6285b188c29c7daafe7f8975860
Tech-A66 <tech-a66@Tech-A66s-MacBook-Air.local>
2017-11-01 08:56:55 +0000

    It works

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 alert("Would you like to play a game?")
 alert("The Rules are: Rock beats Scissors, Paper Beats Rock and Scissors Beats Paper. Best of 5 rounds win")
 
-var playerScore = 0
-var compScore = 0
+var playerScore = 0;
+var compScore = 0;
 
 var g = 1;
 while (g < 6) {
@@ -10,11 +10,11 @@ while (g < 6) {
 var playerChoice = prompt("Choose Rock, Paper or Scissors")
 var compChoice = Math.random();
   if (compChoice < 0.34){
-    compChoice = "Rock"
+    compChoice = "Rock";
   }if (compChoice <= 0.67){
-    compChoice = "Paper"
+    compChoice = "Paper";
   }else{
-    compChoice = "Scissors"
+    compChoice = "Scissors";
   }
 
 var battle = function(weapon1, weapon2){
@@ -22,30 +22,48 @@ var battle = function(weapon1, weapon2){
     return alert("Its a tie")
   }else if (weapon1 === "Rock"){
     if (weapon2 === "Scissors"){
-      return alert("Player 1 Wins!") playerScore = ++
+      playerScore += 1;
+      return alert("Player 1 Wins! Player Score" + playerScore);
   }else{
-      return alert("Computer Wins") 
+      compScore += 1;
+      return alert("Computer Wins Computer Score " + compScore)
     }
 
   }else if (weapon1 === "Paper"){
     if (weapon2 === "Rock"){
-      return alert("Player 1 Wins") playerScore = ++
+      playerScore += 1;
+      return alert("Player 1 Wins")
     }else{
+      compScore += 1;
       return alert("Computer Wins")
     }
 
   }else if (weapon1 === "Scissors"){
     if (weapon2 === "Paper"){
-      return alert("Player 1 Wins") playerScore = ++
+      playerScore += 1;
+      return alert("Player 1 Wins")
     }else{
+      compScore += 1;
       return alert("Computer Wins")
     }
   }
 }
 
+
 battle(playerChoice, compChoice);
-alert("The score is")
+alert("The score is: Player1: " + playerScore + " vs. Computer: " + compScore);
 g++
+
+}
+var Winner = function(Score1, Score2) {
+  if (Score1 === Score2) {
+    return gameScore = "tie!, You're both losers"
+  } else if (Score1 > Score2){
+    return gameScore = "You win. Well done!"
+  }else {
+    return gameScore = "You lost to a computer, you suck"
+  }
 }
 
-alert("The winner is:")
+Winner(playerScore, compScore);
+alert("The winner is:" + gameScore)
